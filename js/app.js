469e0cb03c73ae9ab9b0783656bb2579f09591cc
bdavijit <bdavijit@gmail.com>
2022-03-11 11:56:51 +0600

    fixed display error

--- js/app.js
+++ js/app.js
@@ -44,12 +44,14 @@ const switchTab = (id) => {
     document.getElementById("liked").style.display = "block";
     document.getElementById("posts").style.display = "none";
     document.getElementById("reported").style.display = "none";
+    document.getElementById("articleBOX").style.display = "none";
 
     displayLikedPosts();
   } else {
     document.getElementById("reported").style.display = "block";
     document.getElementById("posts").style.display = "none";
     document.getElementById("liked").style.display = "none";
+    document.getElementById("articleBOX").style.display = "none";
 
     displayReportedPosts();
   }
